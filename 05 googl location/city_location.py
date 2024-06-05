cfe6c6d5ef5af812b98725ef3b390f91185af592
Sammy508 <chapagainsamir9@gmial.com>
2024-06-05 07:11:54 +0545

    map

--- 05 googl location/city_location.py
+++ 05 googl location/city_location.py
@@ -1,13 +1,14 @@
 import webbrowser
 
-def find_city(city_name):
+def find_city(city_name,street):
 
     # format the url eith google search
 
-    google_map_url = f'https://earth.google.com/web/search/{city_name}'
+    google_map_url = f'https://earth.google.com/web/search/{city_name}/{street}'
 
     webbrowser.open(google_map_url)
 
     # enter city name
 city_name = input("enter city name :")
-find_city(city_name)    
+street= input('enter street name:')
+find_city(city_name,street)    
